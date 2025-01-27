63691c92aad2d6169abdadd84d8b5db148feb131
lizkisssss <lizasalamakha2010@gmail.com>
2025-01-27 12:18:35 +0200

    practice-lesson-02

---
+++ js/vars-types-02.js
@@ -0,0 +1,15 @@
+const speed = 120;
+const distance = 617.54;
+const isAdmin = true;
+
+const congratulation = "Привіт вітаємо на нашому сайті";
+
+alert(congratulation);
+
+const myName = "Яке твоє ім'я?"
+const username = prompt(myName)
+console.log(username)
+console.log(typeof username)
+
+const shouldrenew = confirm("Чи хочете продовжити підписку?");
+console.log(shouldrenew)
